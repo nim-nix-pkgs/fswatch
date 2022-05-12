bf0c36dc97355341fa521f93cea6c87239f66191
hugosenari <hugosenari@gmail.com>
2022-05-12 11:51:44 -0300

    chore: re index fswatch

--- flake.nix
+++ flake.nix
@@ -7,6 +7,22 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."fswatch-master".dir   = "master";
+  inputs."fswatch-master".owner = "nim-nix-pkgs";
+  inputs."fswatch-master".ref   = "master";
+  inputs."fswatch-master".repo  = "fswatch";
+  inputs."fswatch-master".type  = "github";
+  inputs."fswatch-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fswatch-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."fswatch-0_1_0".dir   = "0_1_0";
+  inputs."fswatch-0_1_0".owner = "nim-nix-pkgs";
+  inputs."fswatch-0_1_0".ref   = "master";
+  inputs."fswatch-0_1_0".repo  = "fswatch";
+  inputs."fswatch-0_1_0".type  = "github";
+  inputs."fswatch-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."fswatch-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
